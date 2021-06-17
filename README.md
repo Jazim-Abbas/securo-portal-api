55b47bde7af8e9305d7f7d4fd6a0fe05fecbdf5a
Abdul Muqeet Arshad <55202773+muqeetchaudhary@users.noreply.github.com>
2021-06-17 16:51:33 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# securo-portal-api
+# Securo Portal RestApi
+## Routes for User
+### to register a new user
+- user/register : post
+```
+{
+    name:
+    email:
+    password:
+}
+```
+
+### to login an existing user
+- user/login :post
+```
+{
+    email:
+    password:
+}
+```
+
+### to view the profile of an authenticated user
+- user/profile :post
---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const { connection } = require("./db/connection")
+const user = require("./routes/user")
+const cors = require("cors")
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use("/user", user)
+
+module.exports = { app }
