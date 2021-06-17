c569eb8610092e09f2d17df066d69d6bed244c31
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-06-17 17:07:58 +0500

    Initial setup for user register and login

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const cors = require("cors")
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(cors())
 
 app.use("/user", user)
 
