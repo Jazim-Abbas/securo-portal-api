2a98c333bcd2f04a8167e3fa492c136525f3b9a7
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-06-18 10:19:19 +0500

    added route for step

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const { connection } = require("./db/connection")
 const user = require("./routes/user")
+const step = require("./routes/step")
 const cors = require("cors")
 
 const app = express()
@@ -11,5 +12,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use("/user", user)
+app.use("/step", step)
 
 module.exports = { app }
---
+++ controllers/step.js
@@ -0,0 +1,7 @@
+const { Step } = require("../db/models/step")
+const Exceptions = require("../utils/custom-exceptions")
+const { promise } = require("../middlewares/promises")
+
+exports.addStep = promise( async (req, res) => {
+    
+})
