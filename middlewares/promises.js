1fedd76eb76337f2a6f7d2e925836f61f3ea4cdf
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-18 12:46:14 +0500

    bug fix: catching exceptions if something goes wrong when we deal with unhandle exceptions ..

--- middlewares/promises.js
+++ middlewares/promises.js
@@ -1,12 +1,26 @@
+const Exceptions = require("../utils/custom-exceptions");
+
 exports.promise = (handler) => {
-    return async (req, res, next) => {
-        try {
-            await handler(req, res);
+  return async (req, res, next) => {
+    try {
+      await handler(req, res);
+    } catch (err) {
+      let errors = [];
+      let message = "Server Error";
+      let statusCode = 500;
+      let name = "SERVER_ERROR";
+
+      if (err instanceof Exceptions.HttpError) {
+        if (err instanceof Exceptions.ValidationError) {
+          errors = err.errors;
         }
-        catch (err) {
-            res.status(404).json({
-                message: `${err}`
-            })
-        }
+
+        message = err.message;
+        statusCode = err.statusCode;
+        name = err.name;
+      }
+
+      res.status(statusCode).send({ message, name });
     }
-}
+  };
+};
