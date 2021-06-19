f36b1dcae05bbe54ebf691937fd0ee6fb52d2d2d
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-06-19 12:00:47 +0500

    added validation to add answers for step 2

--- validation/answer.js
+++ validation/answer.js
@@ -1,6 +1,15 @@
 const yup = require("yup")
 
 exports.addAnswerSchema = yup.object({
-    answer: yup.string().required(),
-    questionId: yup.string().required(),
+    stepId: yup.string().required(),
+    answer1: yup.string().required(),
+    questionId1: yup.string().required(),
+    answer2: yup.string().required(),
+    questionId2: yup.string().required(),
+    answer3: yup.string().required(),
+    questionId3: yup.string().required(),
+    answer4: yup.string().required(),
+    questionId4: yup.string().required(),
+    answer5: yup.string().required(),
+    questionId5: yup.string().required(),
 })
