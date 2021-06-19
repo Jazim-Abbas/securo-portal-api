23727608147d76921d50596525ca26ad111dbc5f
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-06-19 16:36:02 +0500

    added route for adding question in section

--- routes/question.js
+++ routes/question.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const question = require("../controllers/question")
 const { authentication } = require("../middlewares/isAuth")
 const { validation } = require("../middlewares/validation")
-const { addquestionSchemaForStep1, addquestionSchemaForStep2, getquestionSchemaForStep } = require("../validation/question")
+const { addquestionSchemaForStep1, addquestionSchemaForStep2, getquestionSchemaForStep, addquestionSchemaForSection2, getquestionSchemaForSection } = require("../validation/question")
 
 router.post("/add-for-step1", validation(addquestionSchemaForStep1), question.addQuestion)
 router.post("/add-for-step2", validation(addquestionSchemaForStep2), question.addQuestion)
+router.post("/add-for-section", validation(addquestionSchemaForSection2), question.addQuestion)
 router.post("/get-for-specificstep", validation(getquestionSchemaForStep), question.getQuestion)
+router.post("/get-for-section", validation(getquestionSchemaForSection), question.getSectionQuestion)
 
 module.exports = router
