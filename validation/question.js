ae773a29f6b867299e34def4440d3c6ab3b157a0
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-06-19 16:35:35 +0500

    added validations for adding question in section

--- validation/question.js
+++ validation/question.js
@@ -11,6 +11,16 @@ exports.addquestionSchemaForStep2 = yup.object({
     stepId: yup.string().required()
 })
 
+exports.addquestionSchemaForSection2 = yup.object({
+    questionTitle: yup.string().required(),
+    questionDescription: yup.string().required(),
+    sectionId: yup.string().required()
+})
+
 exports.getquestionSchemaForStep = yup.object({
     stepId: yup.string().required()
+})
+
+exports.getquestionSchemaForSection = yup.object({
+    sectionId: yup.string().required()
 })
