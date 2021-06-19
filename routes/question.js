1f9fc7cc0d2d673aac2fcef46ca0aceee5deb7ce
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-06-19 12:29:19 +0500

    added route for adding question for step 2

--- routes/question.js
+++ routes/question.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const question = require("../controllers/question")
 const { authentication } = require("../middlewares/isAuth")
 const { validation } = require("../middlewares/validation")
-const { addquestionSchemaForStep, getquestionSchemaForStep } = require("../validation/question")
+const { addquestionSchemaForStep1, addquestionSchemaForStep2, getquestionSchemaForStep } = require("../validation/question")
 
-router.post("/add-for-step", validation(addquestionSchemaForStep), question.addQuestion)
+router.post("/add-for-step1", validation(addquestionSchemaForStep1), question.addQuestion)
+router.post("/add-for-step2", validation(addquestionSchemaForStep2), question.addQuestion)
 router.post("/get-for-specificstep", validation(getquestionSchemaForStep), question.getQuestion)
 
 module.exports = router
