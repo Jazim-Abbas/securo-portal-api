c7a1b4bad7bf7132dd5f1f4d692c7c034631ff4b
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-06-21 11:02:50 +0500

    added route to add answer for section

--- routes/answer.js
+++ routes/answer.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const answer = require("../controllers/answer")
 const { authentication } = require("../middlewares/isAuth")
 const { validation } = require("../middlewares/validation")
-const { addAnswerSchema } = require("../validation/answer")
+const { addAnswerSchemaForStep, addAnswerSchemaForSection } = require("../validation/answer")
 
-router.post("/add-for-step1", authentication, validation(addAnswerSchema), answer.addAnswerForStep1)
-router.post("/add-for-step2", authentication, validation(addAnswerSchema), answer.addAnswerForStep2)
+router.post("/add-for-step1", authentication, answer.addAnswerForStep1)
+router.post("/add-for-step2", authentication, answer.addAnswerForStep2)
+router.post("/add-for-section", authentication, answer.addAnswerForSection)
 
 module.exports = router
