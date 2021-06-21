4078762fc8613e9750b49e7d55508ffba618918c
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-06-21 11:03:04 +0500

    added controller to add answer for section

--- controllers/answer.js
+++ controllers/answer.js
@@ -8,80 +8,37 @@ exports.addAnswerForStep1 = promise(async (req, res) => {
 })
 
 exports.addAnswerForStep2 = promise(async (req, res) => {
-    // res.json("Add answers for step 2")
 
     const body = req.body
 
-    const answer = await Answer.findOne({stepId: body.stepId, userId: req.user._id})
+    const question = await Question.find({ stepId: body[0].stepId })
+    if (!question) throw new Exceptions.NotFound
 
-    if(answer) throw new Exceptions.BadRequset("You have already submitted answers for this step")
+    console.log(question.length)
 
-    const newAnswer = ([
-        {
-            answer: body.answer1,
-            questionId: body.questionId1,
-            userId: req.user._id,
-            stepId: body.stepId
-        },
-        {
-            answer: body.answer2,
-            questionId: body.questionId2,
-            userId: req.user._id,
-            stepId: body.stepId
-        },
-        {
-            answer: body.answer3,
-            questionId: body.questionId3,
-            userId: req.user._id,
-            stepId: body.stepId
-        },
-        {
-            answer: body.answer4,
-            questionId: body.questionId4,
-            userId: req.user._id,
-            stepId: body.stepId
-        },
-        {
-            answer: body.answer5,
-            questionId: body.questionId5,
-            userId: req.user._id,
-            stepId: body.stepId
-        }
-    ])
+    const answer = await Answer.findOne({ stepId: body[0].stepId, userId: req.user._id })
 
-    Answer.insertMany(newAnswer)
+    if (answer) throw new Exceptions.BadRequset("You have already submitted answers for this step")
+
+    Answer.insertMany(body)
 
     res.status(200).json("Successfully Inserted Answers")
+})
 
-    // const question = await Question.find({ stepId: body.stepId })
-    // if (!question) throw new Exceptions.NotFound
+exports.addAnswerForSection = promise(async (req, res) => {
 
-    // console.log(question.length)
+    const body = req.body
 
-    // for (var i = 1; i <= question.length; i++) {
+    const question = await Question.find({ sectionId: body[0].sectionId })
+    if (!question) throw new Exceptions.NotFound
 
-    //     console.log(body.answer1);
-    //     console.log(`body.answer${i}`)
-    //     answer + i
-    //     "answer" + i
+    console.log(question.length)
 
-    //     var answer = `body.answer${i}`
-    //     var questionId = `body.questionId${i}`
+    const answer = await Answer.findOne({ userId: req.user._id, sectionId: body[0].sectionId })
 
-    //     console.log(typeof(body.questionId + i))
+    if (answer) throw new Exceptions.BadRequset("You have already submitted answers for this section")
 
-    //     var newAnswer = await [(
-    //         {
-    //             answer: body.answer+i,
-    //             questionId: body.questionId+i,
-    //             userId: req.user._id
-    //         }
-    //     )]
+    Answer.insertMany(body)
 
-    //     console.log(newAnswer)
-
-    //     // res.status(200).json("Successfully Inserted Answers")
-    // }
-
-
+    res.status(200).json("Successfully Inserted Answers")
 })
