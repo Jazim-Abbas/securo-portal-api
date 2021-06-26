a990f22e09724544d006be21c085f64a2b47dcb6
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-06-26 11:24:08 +0500

    added array to find sections

--- controllers/section.js
+++ controllers/section.js
@@ -13,7 +13,7 @@ exports.addSection = promise(async (req, res) => {
 
 exports.getSection = promise(async (req, res) => {
     const body = req.body
-    const section = await Section.find({ serviceId: { $in: [body.serviceId1, body.serviceId2, body.serviceId3, body.serviceId4] } })
+    const section = await Section.find({ serviceId: { $in: body } })
     if (!section) throw new Exceptions.NotFound
 
     res.status(200).json({ section })
